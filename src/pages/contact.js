ba27e3e7692d4058b251ee701d907849eb49a857
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-09-05 16:55:16 -0500

    🧹

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react"
-import styled from "styled-components"
-import { FormattedMessage, Link, useIntl } from "gatsby-plugin-intl"
 
 import MainLayout from "../components/MainLayout";
-import ContactForm from "../components/ContactForm";
 import SEO from "../components/SEO";
 
-import { QUERIES, WEIGHTS } from "../constants";
 import ContactFormComponent from "../components/ContactForm/ContactFormComponent";
 
 export default function ContactPage() {
@@ -17,7 +13,7 @@ export default function ContactPage() {
         title={`Matt Oliver`}>
       </SEO>
       <MainLayout>
-        {/* <ContactForm /> */}
+
         <ContactFormComponent />
       </MainLayout>
     </>
