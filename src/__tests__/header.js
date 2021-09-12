7a2f3fcebb517017126d41f711e70f5a56af4587
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-09-12 12:17:21 -0500

    adding tests

--- src/__tests__/header.js
+++ src/__tests__/header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 
-import PageHero from '../components/PageHero'
+import MagicHeader from '../components/MagicHeader'
 
 // You have to write data-testid
 const TestTitle = () => <h1 data-testid="test-title">Testing is great!</h1>
