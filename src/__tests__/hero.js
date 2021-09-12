0409cef1b7742c860397e01c61f06735c01ac44f
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-09-12 12:06:37 -0500

    add module mapper

--- src/__tests__/hero.js
+++ src/__tests__/hero.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 
+import PageHero from '../components/PageHero'
+
 // You have to write data-testid
 const TestTitle = () => <h1 data-testid="test-title">Testing is great!</h1>
 
-test("Displays the correct title", () => {
+test("The dummy test displays the correct title", () => {
   const { getByTestId } = render(<TestTitle />)
   // Assertion
-  expect(getByTestId("test-title")).toHaveTextContent("Gatsby is awesome!")
+  expect(getByTestId("test-title")).toHaveTextContent("Testing is great!")
   // --> Test will pass
 })
