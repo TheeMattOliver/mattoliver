8398c69cd65f6986deff95f384876e46477206b5
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-10-24 12:16:49 -0500

    wip :construction:

--- src/pages/d3-react-hooks/bar-normalized-census.js
+++ src/pages/d3-react-hooks/bar-normalized-census.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import * as d3 from "d3"
-import { nest, key, rollup, entries, map } from "d3-collection"
 
 import { QUERIES } from "../../constants"
 import ChartPage from "../../templates/ChartPage"
@@ -13,32 +12,9 @@ nisi ut aliquip ex ea commodo consequat.`
 
 export default function BarNormalizedCensusPage() {
   return (
-    <ChartPage title={`Bar chart and the Census API`} copy={copy}>
-      {censusData ? (
-        <BarChartPopulation data={censusData} />
-      ) : (
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      )}
-
-      <ButtonWrapper>
-        <StateSelect value={selectedState} onChange={handleChange}>
-          {statesArr.map((option, index) => (
-            <option key={index} value={option}>
-              {option}{" "}
-            </option>
-          ))}
-        </StateSelect>
-      </ButtonWrapper>
-    </ChartPage>
+    <ChartPage
+      title={`Normalized Bar chart and the Census API`}
+      copy={copy}
+    ></ChartPage>
   )
 }
-const ButtonWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  /* justify-content: space-between; */
-  padding-right: 1.67rem;
-  padding-bottom: 2rem;
-`
-const StateSelect = styled(Select)``
