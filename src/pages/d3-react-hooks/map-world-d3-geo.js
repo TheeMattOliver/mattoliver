a93dbfbedbc75306f309843f4a7049088c639e77
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-10-24 14:23:56 -0500

    d3 geo

--- src/pages/d3-react-hooks/map-world-d3-geo.js
+++ src/pages/d3-react-hooks/map-world-d3-geo.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react"
 import * as d3 from "d3"
+import styled from "styled-components"
 
 import MapWorldGeo from "../../components/D3ReactHooks/MapWorldGeo"
+import ChartPage from "../../templates/ChartPage"
+import Select from "../../components/Select"
 
 export default function MapWorldGeoPage() {
   const [property, setProperty] = useState("pop_est")
@@ -22,15 +25,29 @@ export default function MapWorldGeoPage() {
   }
   return (
     <>
-      <MapWorldGeo data={data} property={property} />
-      <select
-        value={property}
-        onChange={event => setProperty(event.target.value)}
-      >
-        <option value="pop_est">Population</option>
-        <option value="name_len">Name length</option>
-        <option value="gdp_md_est">GDP</option>
-      </select>
+      <ChartPage>
+        <MapWorldGeo data={data} property={property} />
+
+        <ButtonWrapper>
+          <Select
+            value={property}
+            onChange={event => setProperty(event.target.value)}
+          >
+            <option value="pop_est">Population</option>
+            <option value="name_len">Name length</option>
+            <option value="gdp_md_est">GDP</option>
+          </Select>
+        </ButtonWrapper>
+      </ChartPage>
     </>
   )
 }
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  /* justify-content: space-between; */
+  padding-right: 1.67rem;
+  padding-bottom: 2rem;
+  margin-top: 3rem;
+`
