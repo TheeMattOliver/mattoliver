bdae7ef887e03e00ab38d4c6a949f7f6a1c9f655
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-10-25 10:08:50 -0500

    implement us capitals map and world airports map

--- src/pages/d3-react-hooks/map-us-capitals.js
+++ src/pages/d3-react-hooks/map-us-capitals.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react"
 import * as d3 from "d3"
 import styled from "styled-components"
 import MapUSCapitals from "../../components/D3ReactHooks/MapUSCapitals"
+import ChartPage from "../../templates/ChartPage"
+
+const copy = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat.`
 
 export default function MapCapitalsPage() {
   const [us, setUS] = useState("")
@@ -24,9 +30,14 @@ export default function MapCapitalsPage() {
 
   return (
     <>
-      {!us && <h1>Loading...</h1>}
+      <ChartPage
+        copy={copy}
+        title={`Albers projection and geocoded points on a map`}
+      >
+        {!us && <h1>Loading...</h1>}
 
-      <MapUSCapitals cities={cities} us={us} />
+        <MapUSCapitals cities={cities} us={us} />
+      </ChartPage>
     </>
   )
 }
