83af18db2627db65f43146fcc2de7556ec470204
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-01-15 10:17:22 -0600

    improve line-height clamp value

--- src/pages/about.js
+++ src/pages/about.js
@@ -178,7 +178,7 @@ const HeroCopyText = styled.p`
   color: var(--color-textPrimary);
   margin-top: 2.75rem;
   padding: 0 1rem;
-  line-height: clamp(1.5rem, /* 1.3vw + .9rem, */ 1.75vw + 0.5rem, 1.95rem);
+  line-height: clamp(2.15rem, /* 1.3vw + .9rem, */ 1.75vw + 0.5rem, 1.95rem);
   font-size: clamp(1.5rem, /* 1.3vw + .9rem, */ 1.75vw + 0.5rem, 1.95rem);
   a {
     color: var(--color-textLink);
